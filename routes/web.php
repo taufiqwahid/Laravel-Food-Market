0f35e08e14e7c28dd5f71aed90d29689bd968aab
Taufiq Wahid <taufiq.wahid58@gmail.com>
2021-11-20 16:55:44 +0800

    User controller and indx

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\MidtransController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,11 +22,12 @@ Route::get('/', function () {
 });
 
 // DASHBOARD
-Route::prefix('dashboard')->middleware('auth:sanctum', 'admin')->group(
-    function () {
-        return Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-    }
-);
+Route::prefix('dashboard')
+    ->middleware(['auth:sanctum', 'admin'])
+    ->group(function () {
+        Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+        Route::resource('users', UserController::class);
+    });
 
 // Midtrans related
 Route::get('midtrans/success', [MidtransController::class, 'success']);
