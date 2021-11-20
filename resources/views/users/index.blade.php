ab4aa6b50849b37aa31c289b908a08c74b498c2e
Taufiq Wahid <taufiq.wahid58@gmail.com>
2021-11-20 17:18:40 +0800

    genarate css tailwind

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -11,6 +11,39 @@
                 <a href="{{ route('users.create') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
                 + Create User</a>
             </div>
+            <div class="bg-white">
+                <table class="table-auto w-full">
+                    <thead>
+                        <tr>
+                            <th class="border px-6 py-4">ID</th>
+                            <th class="border px-6 py-4">Name</th>
+                            <th class="border px-6 py-4">Email</th>
+                            <th class="border px-6 py-4">Roles</th>
+                            <th class="border px-6 py-4">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($user as $item)
+                        <tr>
+                            <th class="border px-6 py-4">{{ $item->id }}</th>
+                            <th class="border px-6 py-4">{{ $item->name }}</th>
+                            <th class="border px-6 py-4">{{ $item->email }}</th>
+                            <th class="border px-6 py-4">{{ $item->roles }}</th>
+                            <th class="border px-6 py-4">
+                                <a href="{{ route('users.edit', $item->id) }}" class="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mx-2 rounded ">
+                                Edit
+                                </a>
+                                <form action="{{ route('users.destroy', $item->id) }}" class="inline-block" method="POST">
+                                    {!! method_field('delete') . csrf_field() !!}
+                                    <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mx-2 rounded">Delete</button>
+                                </form>
+                            </th>
+                                
+                        </tr>
+                            @endforeach
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </x-app-layout>
