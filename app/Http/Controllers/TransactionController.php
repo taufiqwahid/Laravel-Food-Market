64f0e2700ebac65439219c829d464382eb4354e1
Taufiq Wahid <taufiq.wahid58@gmail.com>
2021-11-22 09:19:29 +0800

    CRUD Transaction

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -47,9 +47,11 @@ class TransactionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Transaction $transaction)
     {
-        //
+        return view('transactions.detail', [
+            'item' => $transaction
+        ]);
     }
 
     /**
@@ -60,7 +62,6 @@ class TransactionController extends Controller
      */
     public function edit($id)
     {
-        //
     }
 
     /**
@@ -81,8 +82,19 @@ class TransactionController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Transaction $transaction)
     {
-        //
+        $transaction->delete();
+        return redirect()->route('transactions.index');
+    }
+
+    public function changeStatus(Request $request, $id, $status)
+    {
+        $transaction = Transaction::findOrFail($id);
+
+        $transaction->status = $status;
+        $transaction->save();
+
+        return redirect()->route('transactions.show', $id);
     }
 }
